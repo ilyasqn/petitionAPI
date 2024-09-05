b17c1921e3ccbf0b52cc34422537d1af4b1d64ce
Ilyas <ilyas.shamshiden@mail.ru>
2024-09-05 06:18:05 +0500

    change authentication permission for reading

---
+++ api/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    """
+    Разрешает доступ только аутентифицированным пользователям на запись.
+    Чтение доступно для всех.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user and request.user.is_authenticated
